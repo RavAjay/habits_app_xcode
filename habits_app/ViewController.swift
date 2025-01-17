43e12af46122dc501ea35e3ca41dbe047b55da49
RavAjay <ravicanada7@gmail.com>
2025-01-16 18:57:14 -0800

    Initial Commit

---
+++ habits_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  habits_app
+//
+//  Created by Home PC on 1/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ habits_appTests/habits_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  habits_appTests.swift
+//  habits_appTests
+//
+//  Created by Home PC on 1/16/25.
+//
+
+import Testing
+@testable import habits_app
+
+struct habits_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
