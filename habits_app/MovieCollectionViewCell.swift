4a4635bec11506940c99e0321caf3b904bee47f2
RavAjay <ravicanada7@gmail.com>
2025-02-22 20:30:10 -0800

    viewcell
    add viewcell

---
+++ habits_app/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  habits_app
+//
+//  Created by Home PC on 2/13/25.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titlelbl: UILabel!
+    @IBOutlet weak var movieimageview: UIImageView!
+}
